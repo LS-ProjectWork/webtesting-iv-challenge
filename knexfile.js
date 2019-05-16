2801445375eec1dd62dda23f6015d6a9d02b3aa5
Darien Payton <darien.payton71@gmail.com>
2019-05-16 16:11:09 -0400

    updated config for testing

--- knexfile.js
+++ knexfile.js
@@ -2,43 +2,15 @@
 
 module.exports = {
 
-  development: {
+  testing: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/testDb.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
     }
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
 
 };
